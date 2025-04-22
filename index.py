ed382284db2ace380aff8e7a30a3bfc98adc649d
Avsanka <nikita555534@gmail.com>
2025-04-22 22:37:30 +0600

    Сделаны полноценные фильтры на главной странице и поправлена функция скачивания таблиц

--- index.py
+++ index.py
@@ -26,7 +26,16 @@ class myDbConnection:
 @app.route('/catches', methods=['GET'])
 def allCatches():
         if request.method == 'GET':
-            return render_template("index.html", now=datetime.now())
+            with myDbConnection().connect() as db:
+                cur = db.cursor()
+                cur.execute(f"select * from districts")
+                districts = cur.fetchall()
+                cur.execute(f"select * from stations")
+                stations = cur.fetchall()
+                cur.execute(f"select * from zones")
+                zones = cur.fetchall()
+            return render_template("index.html", now=datetime.now(), districts=districts, zones=zones,
+                                   stations=stations)
 
 @app.route('/health', methods=['GET'])
 def healthCheck():
@@ -44,14 +53,48 @@ def sortCatches(year, month):
         else:
             return catches
 
+@app.route('/sortCatches', methods=['POST'])
+def filteredCatches():
+    data = request.get_json()
+    headers = data[0]
+    filters = data[1]
+    new_headers = []
+    new_filters = []
+
+    for i in range(len(filters)):
+        if filters[i] != '0':
+            new_headers.append(headers[i])
+            new_filters.append(filters[i])
+    data = [new_headers, new_filters]
+
+    query = "select * from catch where"
+    for i in range(len(data[0])):
+        query += f" {data[0][i]} = {data[1][i]} and"
+    query = query[:-4]
+    print(query)
+    default = [{'empty': 0}]
+    with myDbConnection().connect() as db:
+        cur = db.cursor()
+        cur.execute(query)
+        catches = cur.fetchall()
+        if len(catches) == 0:
+            return default, http.HTTPStatus(200)
+        else:
+            return catches, http.HTTPStatus(200)
+
+
+
+
 @app.route('/catchDetails/<int:catchID>', methods=['GET'])
 def catchDetails(catchID):
     with myDbConnection().connect() as db:
         cur = db.cursor()
-        cur.execute(f"SELECT ID_Catch, Date, zones.Name as Zone, stations.Name as Station, "
+        cur.execute(f"SELECT ID_Catch, Date, zones.Name as Zone, stations.Name as Station, districts.Name as District, "
                     f"Biotope, Place, Coords_X, Coords_Y, Traps_Amount, Catched_Amount, Comments"
                     f" FROM `catch` left join zones on catch.Zone_ID = zones.ID_Zone "
-                    f"left join stations on catch.Station_ID = stations.ID_Station WHERE ID_Catch = {catchID}")
+                    f"left join stations on catch.Station_ID = stations.ID_Station "
+                    f"left join districts on catch.District_ID = districts.ID_District "
+                    f"WHERE ID_Catch = {catchID}")
         catch = cur.fetchone()
         return render_template('catchDetails.html', catch=catch)
 
@@ -81,12 +124,15 @@ def addCatch():
             stations = cur.fetchall()
             cur.execute(f"select * from zones")
             zones = cur.fetchall()
-            return render_template("addCatch.html", zones=zones, stations=stations)
+            cur.execute(f"select * from districts")
+            districts = cur.fetchall()
+            return render_template("addCatch.html", zones=zones, stations=stations, districts=districts)
     else:
         date = request.form.get('date', 0)
         zone = request.form.get('zone', 0)
         station = request.form.get('station', 0)
         biotope = request.form.get('biotope', 0)
+        district = request.form.get('district', 0)
         place = request.form.get('place', 0)
         cx = request.form.get('cx', 0)
         cy = request.form.get('cy', 0)
@@ -96,8 +142,8 @@ def addCatch():
         try:
             with myDbConnection().connect() as db:
                 cur = db.cursor()
-                cur.execute(f"insert into catch(Date, Zone_ID, Station_ID, Biotope, Place, Coords_X, Coords_Y, Traps_Amount, Catched_Amount, Comments)"
-                            f"values ('{date}', '{zone}', '{station}', '{biotope}', '{place}', '{cx}', '{cy}', '{traps}', '{catched}', '{comments}');")
+                cur.execute(f"insert into catch(Date, Zone_ID, Station_ID, Biotope, District_ID, Place, Coords_X, Coords_Y, Traps_Amount, Catched_Amount, Comments)"
+                            f"values ('{date}', '{zone}', '{station}', '{biotope}', '{district}', '{place}', '{cx}', '{cy}', '{traps}', '{catched}', '{comments}');")
                 db.commit()
                 return "success", http.HTTPStatus(200)
         except:
@@ -190,11 +236,14 @@ def editCatch(catchID):
             stations = cur.fetchall()
             cur.execute(f"select * from zones")
             zones = cur.fetchall()
-            return render_template("editCatch.html", catch=catch, stations=stations, zones=zones)
+            cur.execute(f"select * from districts")
+            districts = cur.fetchall()
+            return render_template("editCatch.html", catch=catch, stations=stations, zones=zones, districts=districts)
     date = request.form.get('date', 0)
     zone = request.form.get('zone', 0)
     station = request.form.get('station', 0)
     biotope = request.form.get('biotope', 0)
+    district = request.form.get('district', 0)
     place = request.form.get('place', 0)
     cx = request.form.get('cx', 0)
     cy = request.form.get('cy', 0)
@@ -205,7 +254,8 @@ def editCatch(catchID):
         with myDbConnection().connect() as db:
             cur = db.cursor()
             cur.execute(f"update catch set Date = '{date}', Zone_ID = '{zone}', Station_ID = '{station}', "
-                        f"Biotope = '{biotope}', Place = '{place}', Coords_X = '{cx}', Coords_Y = '{cy}', "
+                        f"Biotope = '{biotope}', District_ID = '{district}', Place = '{place}', Coords_X = '{cx}', "
+                        f"Coords_Y = '{cy}', "
                         f"Traps_Amount = '{traps}', Catched_Amount = '{catched}', Comments = '{comments}' "
                         f"where ID_Catch = {catchID};")
             db.commit()
@@ -228,24 +278,23 @@ def delMiceList(catchID):
 @app.route('/downloadExcel/<int:catchID>', methods=['POST'])
 def downloadExcel(catchID):
     data = request.get_json()
-    # print(data[-1])
-    # print(data)
     df = pd.DataFrame(data[0:-2], columns=data[-2])
-    # metrics_df = pd.DataFrame(["hallo", "amigo"], columns=['col1'])
+    add_data_df = pd.DataFrame([data[-1]], columns=['Дата отлова: ', 'Район: ', 'Место: '])
     output = io.BytesIO()
+
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        # metrics_df.to_excel(writer, index=False, sheet_name='Data', startrow=0)
-        df.to_excel(writer, index=False, sheet_name='Data')
+        add_data_df.to_excel(writer, index=False, sheet_name='Data', startrow=0)
+        df.to_excel(writer, index=False, sheet_name='Data', startrow=2)
         for column in df:
             column_width = max(df[column].astype(str).map(len).max(), len(column)) + 3
             col_idx = df.columns.get_loc(column)
             writer.sheets['Data'].set_column(col_idx, col_idx, column_width)
+        writer.sheets['Data'].set_column(0, 3, 20)
 
     output.seek(0)
     return send_file(output, download_name=f"data.xlsx", as_attachment=True), http.HTTPStatus(200)
 
 
-#catch'и с сортировкой по дате
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8081)
